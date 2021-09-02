730411bd1a31a899495265700c274562a230bd5f
mazarinno <mcsfakianos@gmail.com>
2021-09-01 22:50:59 -0500

    courses component

--- client/src/components/courses.js
+++ client/src/components/courses.js
@@ -1,44 +1,54 @@
-import React from 'react'
+import React, { Component} from 'react'
 
-const Courses = ({ courses }) => {
-  return (
-    <div id="root">
-      <header>
-        <div class="wrap header--flex">
-          <h1 class="header--logo"><a href="index.html">Courses</a></h1>
-          <nav>
-            <ul class="header--signedout">
-              <li><a href="sign-up.html">Sign Up</a></li>
-              <li><a href="sign-in.html">Sign In</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main>
-        <div class="wrap main--grid">
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Build a Basic Bookcase</h3>
-          </a>
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Learn How to Program</h3>
-          </a>
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Learn How to Test Programs</h3>
-          </a>
-          <a class="course--module course--add--module" href="create-course.html">
-            <span class="course--add--title">
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-            viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-            New Course
-            </span>
-                </a>
-            </div>
+class Courses extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/courses')
+      .then(response => response.json())
+      .then(data => this.setState({ courses: data }))
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <div id="root">
+        <header>
+          <div className="wrap header--flex">
+            <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+            <nav>
+              <ul className="header--signedout">
+                <li><a href="sign-up.html">Sign Up</a></li>
+                <li><a href="sign-in.html">Sign In</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <main>
+          <div className="wrap main--grid">
+          {this.state.courses.map((course) =>
+              <a className="course--module course--link" href="course-detail.html" key={course.id}>
+                <h2 className="course--label">Course</h2>
+                <h3 className="course--title">{course.title}</h3>
+              </a>
+          )}
+            <a className="course--module course--add--module" href="create-course.html">
+              <span className="course--add--title">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+              viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+              New Course
+              </span>
+            </a>
+          </div>
         </main>
-    </div>
-  )
+      </div>
+    )
+  }
 };
 
 export default Courses;
