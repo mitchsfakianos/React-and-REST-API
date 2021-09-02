71a30768a7c5a4db08244c7260abac21dc4a61a7
mazarinno <mcsfakianos@gmail.com>
2021-09-01 19:46:09 -0500

    components 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
+import './styles/global.css';
 import React, {Component} from 'react';
 import Courses from './components/courses';
-
+/*import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import Header from './components/Header';
+import UserSignOut from './components/UserSignOut';
+*/
 export default class App extends Component {
 	render () {
 		return (
-		  <h1>Hi</h1>
+		  <Courses/>
 		)
 	}
-
-
 }
